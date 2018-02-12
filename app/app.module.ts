6f1b7cc8e89e1493b3f5f8c18f2dcbe9bda3a444
JohnS <john@groovygecko.com>
2018-02-12 17:28:49 +0000

    Latest changes

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import { EventsAppComponent} from "./events-app.component";
+import {EventsListComponent} from "./events/events-list.component";
+import {NavbarComponent} from "./nav/navbar.component";
+import {EventThumbnailComponent} from "./events/event-thumbnail.component";
+import {EventService} from "./events/shared/event.service";
+import {ToastrService} from "./common/toastr.service";
+import {EventDetailsComponent} from "./events/event-details/event-details.component";
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./routes";
+
+@NgModule({
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+    declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent, EventDetailsComponent],
+    providers: [EventService, ToastrService],
+    bootstrap: [EventsAppComponent]
+})
+
+export class AppModule{
+
+}
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from "@angular/core";
+import {EventService} from "./shared/event.service";
+import {ToastrService} from "../common/toastr.service";
+
+@Component({
+    selector: 'events-list',
+    templateUrl: 'app/events/events-list.component.html'
+})
+
+export class EventsListComponent implements OnInit {
+    events: any[];
+    constructor(private eventService: EventService, private toastr:ToastrService) {
+
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents();
+    }
+
+    handleThumbnailClick(eventName){
+        this.toastr.success(eventName);
+    }
+}
